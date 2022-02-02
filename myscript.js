639cd6a023a45a586f6ab9278abf66cb2c953bf1
dhruvi2207 <98868416+dhruvi2207@users.noreply.github.com>
2022-02-02 12:05:32 +0530

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,41 @@
+const URL="https://covid19.mathdro.id/api";
+
+let app=angular.module("Myapp",[]);
+
+app.controller('MyCtrl',($scope ,$http)=>{
+//this is controller
+
+$scope. title ="stay home stay safe";
+
+console.log("APP loaded");
+//calling api
+
+$http.get(URL).then((response)=>{
+//success
+
+console.log(response);
+$scope.all_data=response.data;
+
+},(error)=>{
+//error
+console.log(error);
+
+}  );
+//get country data
+$scope.get_c_data=()=>{
+   let country=$scope.c;
+   if(country==''){
+       $scope.c_data=undefined;
+       return;
+   }
+   $http.get(`${URL}/countries/${country}`)
+   .then((response)=>{
+       console.log(response.data);
+       $scope.c_data=response.data;
+   },(error)=>{
+       console.log(error);
+   })
+
+};
+
+});
